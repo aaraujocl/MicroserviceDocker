6d746fa4cfb5d4fca1432ad14b7fa602328a4f80
aaraujocl <69397063+aaraujocl@users.noreply.github.com>
2023-05-08 07:31:32 -0500

    api_category push initial

--- web_apiproductdocker/Modelo/Dto/Response.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace web_apiproductdocker.Modelo.Dto
-{
-    public class Response
-    {
-        public bool Result { get; set; }
-        public CategoriaDto Categoria { get; set; }
-        public string Message { get; set; }
-    }
-}
